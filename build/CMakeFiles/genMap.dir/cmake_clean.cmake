ee27e07319eb5f22dd64a351363c14f1e41f7dd4
loujiabin1994 <634227611@qq.com>
2020-11-16 19:33:19 +0800

    python script

---
+++ build/CMakeFiles/genMap.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/genMap.dir/src/genMap.cpp.o"
+  "genMap.pdb"
+  "genMap"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/genMap.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/ljb/catkin_ws/src/Evoluationary_Programmer/build/devel:$CMAKE_PREFIX_PATH"
+export PATH='/opt/ros/melodic/bin:/home/ljb/anaconda3/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/snap/bin'
+export PYTHONPATH='/opt/ros/melodic/lib/python2.7/dist-packages:/home/ljb/test_env_ros/devel/lib/python3/dist-packages/'
+export ROSLISP_PACKAGE_DIRECTORIES='/home/ljb/catkin_ws/src/Evoluationary_Programmer/build/devel/share/common-lisp'
+export ROS_PACKAGE_PATH='/home/ljb/catkin_ws/src/Evoluationary_Programmer:/opt/ros/melodic/share'
