14f6a695caa94f354f9c1c7fa30cd7a90693fb11
loujiabin1994 <634227611@qq.com>
2021-12-04 13:19:16 +0800

    Relese after the paper is accepted

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ljb/catkin_ws/src/evoluationary_programmer
-# Build directory: /home/ljb/catkin_ws/src/evoluationary_programmer/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
